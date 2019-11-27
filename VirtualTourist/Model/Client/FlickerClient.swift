3450fd17060b09514c417f35384deb4a019fc01a
Christopher Ponce Mendez <christopherponcedev@gmail.com>
2019-11-26 21:43:46 -0400

    added the client file and created the view controller files with mvc pattern

---
+++ VirtualTourist/Model/Client/FlickerClient.swift
@@ -0,0 +1,14 @@
+//
+//  FlickerClient.swift
+//  VirtualTourist
+//
+//  Created by Christopher Ponce Mendez on 11/26/19.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+import Foundation
+
+
+class FlickerClient {
+    
+}
